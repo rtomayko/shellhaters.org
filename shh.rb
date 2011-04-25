9b269a4ea835ce7dd4499e69a1d6c786bc21e4f9
Ryan Tomayko <rtomayko@gmail.com>
2011-04-25 05:48:01 -0700

    move slides to /deck, add index page

--- shh.rb
+++ shh.rb
@@ -6,9 +6,33 @@ class SHH < Sinatra::Base
 
   get '/' do
     content_type 'text/html', :charset => 'utf8'
-    File.read('index.html')
+    @title = "The Shell Hater's Handbook"
+    @body_class = 'home'
+    @stylesheets = ['/styles.css']
+    erb :index
   end
 
+  # POSIX shell utilities cheat sheet
+  %w[/posix /cheat].each do |url|
+    get url do
+      content_type 'text/html', :charset => 'utf8'
+      @title = "The Hater's Shortcut to The POSIX Shell and Utilities"
+      @body_class = 'cheat'
+      @stylesheets = ['/styles.css']
+      erb :cheat
+    end
+  end
+
+  # the essay that accompanies the presentation
+  get '/awkward' do
+    content_type 'text/html', :charset => 'utf8'
+    @title = "AWK-ward Ruby"
+    @body_class = 'essay'
+    @stylesheets = ['http://tomayko.com/css/tomayko.css']
+    erb :essay
+  end
+
+  # pass /deck down to Showoff after faking root URLs
   get '/deck' do |url|
     @env['REQUEST_PATH'] = '/'
     @env['PATH_INFO'] = '/'
@@ -16,29 +40,13 @@ class SHH < Sinatra::Base
     forward
   end
 
-  %w[/posix /cheat].each do |url|
-    get url do
-      content_type 'text/html', :charset => 'utf8'
-      @title = "The Hater's Shortcut to The POSIX Shell and Utilities"
-      @body_class = 'cheat'
-      erb :cheat
-    end
+  get '/styles.css' do
+    content_type 'text/css'
+    send_file 'styles.css'
   end
 
   def markdown(file)
     text = File.read(file)
     Markdown.new(text).to_html
-  end
-
-  get '/awkward' do
-    content_type 'text/html', :charset => 'utf8'
-    @title = "AWK-ward Ruby"
-    @body_class = 'essay'
-    erb :essay
-  end
-
-  get '/styles.css' do
-    content_type 'text/css'
-    send_file 'styles.css'
   end
 end
